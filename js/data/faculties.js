8c0295f9e4bc8a3bbd1be1687db2fdb7d688a88c
MazvydasV <mvaicekauskas@inbox.lt>
2022-05-19 19:59:36 +0300

    first task: meteo

--- js/data/faculties.js
+++ js/data/faculties.js
@@ -2,9 +2,11 @@ import Faculty from '../entities/faculty.js';
 
 
 export const infoAndMathFaculty = new Faculty('Informatikos ir matematikos fakultetas');
+export const chemijosIrGeomokslu = new Faculty ('Chemijos ir geomokslų fakultetas');
 
 const faculties = [
   infoAndMathFaculty,
+  chemijosIrGeomokslu,
 ];
 
 export default faculties;
