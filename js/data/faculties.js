5183ea87c47db25ec1aa8d9e792875d39cc00ceb
Vytautas <vytautas.palubinskass@gmail.com>
2022-05-19 19:58:52 +0300

    created_faculty

--- js/data/faculties.js
+++ js/data/faculties.js
@@ -1,10 +1,12 @@
-import Faculty from '../entities/faculty.js';
+import Faculty from "../entities/faculty.js";
 
+export const infoAndMathFaculty = new Faculty(
+  "Informatikos ir matematikos fakultetas"
+);
+export const economicsAndBusinessFaculty = new Faculty(
+  "Ekonomikos ir verslo administravimo fakultetas"
+);
 
-export const infoAndMathFaculty = new Faculty('Informatikos ir matematikos fakultetas');
-
-const faculties = [
-  infoAndMathFaculty,
-];
+const faculties = [infoAndMathFaculty, economicsAndBusinessFaculty];
 
 export default faculties;
