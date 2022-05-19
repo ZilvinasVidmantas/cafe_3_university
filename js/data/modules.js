1d7902e31941b62fff3b191acee599ec7f710574
artgeca <artgeca@gmail.com>
2022-05-19 19:42:58 +0300

    feat:added informatics-engineering modules to modules.js

--- js/data/modules.js
+++ js/data/modules.js
@@ -1,8 +1,9 @@
 import { softwareSystemsModules } from './info/software-systems.js';
+import { informaticsSystemsModules } from './info-math/informatics-engineering.js';
 
 const modules = [
   ...softwareSystemsModules,
-  // ...softwareSystemsModules,
+  ...informaticsSystemsModules
   // ...lawModules,
   // ...biologyModules,
   // ...chemistryModules,
