a4bb27465e1860f14e28d8c3b8c254b746b77a53
MazvydasV <mvaicekauskas@inbox.lt>
2022-05-19 21:54:29 +0300

    feat(6) five new students

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import softwareSystems from './data/info/software-systems.js';
+import meteorologyAndHidrology from './data/chem/meteorology-hidrology.js';
 import modules from './data/modules.js'
 import Student from './entities/student.js';
 
@@ -8,6 +9,14 @@ const studentsInfoAndMathSoftwareSystems = [
   new Student('Bekas', 'Serversaidas', softwareSystems.id),
   new Student('Frontas', 'Klijentauskas', softwareSystems.id),
   new Student('Kompiuterė', 'Komplailerytė', softwareSystems.id),
+];
+
+const studentsChemMeteorologyAndHidrology = [
+  new Student('Nemunas', 'Panemunis', meteorologyAndHidrology.id),
+  new Student('Ieva', 'Drebulaitė', meteorologyAndHidrology.id),
+  new Student('Zosė', 'Kalnienė', meteorologyAndHidrology.id),
+  new Student('Antosė', 'Draugytė', meteorologyAndHidrology.id),
+  new Student('Perkūnas', 'Plonulis', meteorologyAndHidrology.id),
 ];
 
 
