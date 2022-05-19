e3d622fa19584b3b6c8a9addf3bcd980aa4eb038
Gytis Paulauskas <paulauskas.gytis@gmail.com>
2022-05-19 20:06:48 +0300

    feat: added med faculty

--- js/data/faculties.js
+++ js/data/faculties.js
@@ -3,8 +3,11 @@ import Faculty from '../entities/faculty.js';
 
 export const infoAndMathFaculty = new Faculty('Informatikos ir matematikos fakultetas');
 
+export const medicineFaculty = new Faculty('Medicinos fakultetas');
+
 const faculties = [
   infoAndMathFaculty,
+  medicineFaculty,
 ];
 
 export default faculties;
