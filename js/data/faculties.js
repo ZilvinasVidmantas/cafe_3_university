907a45f82f6267ca85ee8299aafd2fce214d2fc8
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2022-05-19 20:01:38 +0300

    feat(1): created new 'medicine' faculty and added it to 'faculties' array

--- js/data/faculties.js
+++ js/data/faculties.js
@@ -2,9 +2,11 @@ import Faculty from '../entities/faculty.js';
 
 
 export const infoAndMathFaculty = new Faculty('Informatikos ir matematikos fakultetas');
+export const medFaculty = new Faculty('Medicinos fakultetas');
 
 const faculties = [
   infoAndMathFaculty,
+  medFaculty
 ];
 
 export default faculties;
