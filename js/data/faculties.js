85e06e5b5b342b8a47b76b27fb7cedd537d84cc4
Romuald Makovskij <romualdasit@gmail.com>
2022-05-19 20:20:00 +0300

    feat: created faculty changes

--- js/data/faculties.js
+++ js/data/faculties.js
@@ -2,7 +2,7 @@ import Faculty from '../entities/faculty.js';
 
 
 export const infoAndMathFaculty = new Faculty('Informatikos ir matematikos fakultetas');
-export const managementAndBusinessAdministrationFaculty = new Faculty('Ekonomikos ir verslo adminstravimo fakultetas');
+export const ecoFaculty = new Faculty('Ekonomikos ir verslo adminstravimo fakultetas');
 
 
 const faculties = [
