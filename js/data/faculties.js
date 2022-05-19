dd6c35d8261f43afe0cde3cea54709bb8c29d96d
kmakselis <kmakselis@gmail.com>
2022-05-19 19:50:30 +0300

    feat(1): created  medicine faculty and added to faculties array

--- js/data/faculties.js
+++ js/data/faculties.js
@@ -2,9 +2,12 @@ import Faculty from '../entities/faculty.js';
 
 
 export const infoAndMathFaculty = new Faculty('Informatikos ir matematikos fakultetas');
+export const medicineFaculty = new Faculty('Medicinos fakultetas');
+
 
 const faculties = [
   infoAndMathFaculty,
+  medicineFaculty,
 ];
 
 export default faculties;
