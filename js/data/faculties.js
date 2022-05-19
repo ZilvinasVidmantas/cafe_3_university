910630396632cccbea8446be034355d1a0f76763
zilvinas-dvareckas <95238038+zdvareckas@users.noreply.github.com>
2022-05-19 19:44:12 +0300

    created philosophy faculty

--- js/data/faculties.js
+++ js/data/faculties.js
@@ -2,9 +2,11 @@ import Faculty from '../entities/faculty.js';
 
 
 export const infoAndMathFaculty = new Faculty('Informatikos ir matematikos fakultetas');
+export const philosophyFaculty = new Faculty('Filosofijos fakultetas');
 
 const faculties = [
   infoAndMathFaculty,
+  philosophyFaculty,
 ];
 
 export default faculties;
