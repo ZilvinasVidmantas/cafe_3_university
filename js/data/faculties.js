add5c96cdf73bdb240d89cdaf353c847bab4f23c
tyckinas <hardyy240@gmail.com>
2022-05-19 20:02:40 +0300

    feat: economics faculty created

--- js/data/faculties.js
+++ js/data/faculties.js
@@ -2,9 +2,11 @@ import Faculty from '../entities/faculty.js';
 
 
 export const infoAndMathFaculty = new Faculty('Informatikos ir matematikos fakultetas');
+export const EconomicsFaculty = new Faculty('Ekonomikos ir verslo administravimo fakultetas');
 
 const faculties = [
   infoAndMathFaculty,
+  EconomicsFaculty
 ];
 
 export default faculties;
