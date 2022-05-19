558bcc4e5485480f55d2c456c88ef957961e242e
artgeca <artgeca@gmail.com>
2022-05-19 19:47:33 +0300

    feat:added 5 informatics systems students in main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import softwareSystems from './data/info/software-systems.js';
+import informaticsSystems from './data/info-math/informatics-engineering.js';
 import modules from './data/modules.js'
 import Student from './entities/student.js';
 
@@ -8,6 +9,14 @@ const studentsInfoAndMathSoftwareSystems = [
   new Student('Bekas', 'Serversaidas', softwareSystems.id),
   new Student('Frontas', 'Klijentauskas', softwareSystems.id),
   new Student('Kompiuterė', 'Komplailerytė', softwareSystems.id),
+];
+
+const studentsInfoAndMathinformaticsSystems = [
+  new Student('Algirdas', 'A', informaticsSystems.id),
+  new Student('Saulius', 'S', informaticsSystems.id),
+  new Student('Tadas', 'T', informaticsSystems.id),
+  new Student('Rima', 'R', informaticsSystems.id),
+  new Student('Rūta', 'R', informaticsSystems.id),
 ];
 
 
