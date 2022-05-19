b22b3e527c1ecde0a1857b60a4d7b22e7f486460
Gytis Paulauskas <paulauskas.gytis@gmail.com>
2022-05-19 20:09:31 +0300

    feat: added 5 students from occupational-therapy

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-import softwareSystems from './data/info/software-systems.js';
-import modules from './data/modules.js'
+import softwareSystems from './data/info-math/software-systems.js';
+import occupationalTherapy from './data/med/occupational-therapy.js';
+import modules from './entities/module.js'
 import Student from './entities/student.js';
 
 const studentsInfoAndMathSoftwareSystems = [
@@ -8,6 +9,14 @@ const studentsInfoAndMathSoftwareSystems = [
   new Student('Bekas', 'Serversaidas', softwareSystems.id),
   new Student('Frontas', 'Klijentauskas', softwareSystems.id),
   new Student('Kompiuterė', 'Komplailerytė', softwareSystems.id),
+];
+
+const studentsOccupationalTherapy = [
+  new Student('Antanas', 'Antanaitis', occupationalTherapy.id),
+  new Student('Virgis', 'Virgilaitis', occupationalTherapy.id),
+  new Student('Jonas', 'Jonaitis', occupationalTherapy.id),
+  new Student('Pijus', 'Pijauskas', occupationalTherapy.id),
+  new Student('Vidas', 'Vidaitis', occupationalTherapy.id),
 ];
 
 
