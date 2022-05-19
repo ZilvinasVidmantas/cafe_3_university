438972e142107698b00ff69172f81b085d9ad7d1
Kristina <kristina.zalandauskaite@gmail.com>
2022-05-19 20:14:49 +0300

    feat:added chem faculty

--- js/data/faculties.js
+++ js/data/faculties.js
@@ -2,9 +2,11 @@ import Faculty from '../entities/faculty.js';
 
 
 export const infoAndMathFaculty = new Faculty('Informatikos ir matematikos fakultetas');
+export const chemistryAndGeosciencesFaculty = new Faculty('Chemijos ir geomokslų fakultetas');
 
 const faculties = [
   infoAndMathFaculty,
+  chemistryAndGeosciencesFaculty,
 ];
 
 export default faculties;
