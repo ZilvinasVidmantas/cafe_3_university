56fae933c0d6084d14e4bd4c88472bab00d3a6d7
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2022-05-19 20:39:08 +0300

    feat(5): added pharmacy to study programs

--- js/data/study-programs.js
+++ js/data/study-programs.js
@@ -1,15 +1,9 @@
 import softwareSystems from "./info/software-systems.js";
+import pharmacy from "./med/pharmacy.js";
 
 const studyPrograms = [
   softwareSystems,
-  // studyProgram2,
-  // studyProgram3,
-  // studyProgram4,
-  // studyProgram5,
-  // studyProgram6,
-  // studyProgram7,
-  // studyProgram8,
-  // ...
+  pharmacy,
 ];
 
 export default studyPrograms;
