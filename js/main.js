c7c0e1677b63087269dd09647d1af95787191fca
Romuald Makovskij <romualdasit@gmail.com>
2022-05-19 21:34:23 +0300

    feat: five students created.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import softwareSystems from './data/info/software-systems.js';
+import managementEnglish from './data/eco/management-english.js';
 import modules from './data/modules.js'
 import Student from './entities/student.js';
 
@@ -9,5 +10,13 @@ const studentsInfoAndMathSoftwareSystems = [
   new Student('Frontas', 'Klijentauskas', softwareSystems.id),
   new Student('Kompiuterė', 'Komplailerytė', softwareSystems.id),
 ];
+
+const studentsManagementEnglis = [
+  new Student('Lina', 'Pakalnyte', managementEnglish.id),
+  new Student('Ona', 'Onyskaite', managementEnglish.id),
+  new Student('Benas', 'Benauskass', managementEnglish.id),
+  new Student('Pavelas', 'Ivanauskas', managementEnglish.id),
+  new Student('Julija', 'Julienė', managementEnglish.id),
+]
 
 
