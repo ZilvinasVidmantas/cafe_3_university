041ddd7550cab7dee31e8101fd501321f8590818
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2022-05-23 19:59:53 +0300

    commit changes

--- js/data/study-programs.js
+++ js/data/study-programs.js
@@ -1,5 +1,5 @@
-import softwareSystems from "./info/software-systems.js";
-import pharmacy from "./med/pharmacy.js";
+import softwareSystems from './info/software-systems.js';
+import pharmacy from './med/pharmacy.js';
 
 const studyPrograms = [
   softwareSystems,
