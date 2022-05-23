048405c387634f17e587d37acfb0caf9bd952214
Juozas Snieška <juozassnieska@Juozass-MacBook-Air.local>
2022-05-23 14:05:42 +0300

    fixed conflicts on study-programs.js

--- js/data/study-programs.js
+++ js/data/study-programs.js
@@ -1,26 +1,9 @@
-<<<<<<< HEAD
-import softwareSystems from "./info/software-systems.js";
-import medicine from "./med/medicine.js";
-
-const studyPrograms = [
-  softwareSystems,
-  medicine,
-  // studyProgram3,
-  // studyProgram4,
-  // studyProgram5,
-  // studyProgram6,
-  // studyProgram7,
-  // studyProgram8,
-  // ...
-=======
-
-
-
 import softwareSystems from "./info-math/software-systems.js";
 import mathematicsAndItsApplications from "./info-math/mathematics-and-its-applications.js";
 import dataScience from "./info-math/data-science.js";
 import businessAndLaw from "./eco/business-law.js";
 import nanoChemistry from "./chem/nano-chemistry.js";
+import medicine from "./med/medicine.js";
   
   const studyPrograms = [
     softwareSystems,
@@ -28,7 +11,7 @@ import nanoChemistry from "./chem/nano-chemistry.js";
     dataScience,
     businessAndLaw,
     nanoChemistry,
->>>>>>> master
+    medicine,
 ];
 
 export default studyPrograms;
