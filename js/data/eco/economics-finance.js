6b4b9e47795de0ac3af36f71a06df4c053861350
tyckinas <hardyy240@gmail.com>
2022-05-23 19:34:50 +0300

    refactor: reformatted files

--- js/data/eco/economics-finance.js
+++ js/data/eco/economics-finance.js
@@ -3,27 +3,26 @@ import StudyProgramSemester from '../../entities/study-program-semester.js';
 import Module from '../../entities/module.js';
 import { economicsAndBusinessFaculty } from '../faculties.js';
 
-const economicsFinance = new StudyProgram("Ekonomika ir finansai" , economicsAndBusinessFaculty.id)
+const economicsFinance = new StudyProgram("Ekonomika ir finansai" , economicsAndBusinessFaculty.id);
 
 const economicsFinanceSemester1 = new StudyProgramSemester();
-const economicsFinanceSemester1module1 = new Module('Dalykinė anglų kalba (C1)', 5)
-const economicsFinanceSemester1module2 = new Module('Duomenų analizės įvadas', 5)
-const economicsFinanceSemester1module3 = new Module('Finansinės apskaitos pagrindai', 5)
-const economicsFinanceSemester1module4 = new Module('Ekonomikos pagrindai', 10)
-const economicsFinanceSemester1module5 = new Module('Matematiniai metodai I/II d.', 5)
+const economicsFinanceSemester1module1 = new Module('Dalykinė anglų kalba (C1)', 5);
+const economicsFinanceSemester1module2 = new Module('Duomenų analizės įvadas', 5);
+const economicsFinanceSemester1module3 = new Module('Finansinės apskaitos pagrindai', 5);
+const economicsFinanceSemester1module4 = new Module('Ekonomikos pagrindai', 10);
+const economicsFinanceSemester1module5 = new Module('Matematiniai metodai I/II d.', 5);
 economicsFinanceSemester1.addModule(economicsFinanceSemester1module1.id);
 economicsFinanceSemester1.addModule(economicsFinanceSemester1module2.id);
 economicsFinanceSemester1.addModule(economicsFinanceSemester1module3.id);
 economicsFinanceSemester1.addModule(economicsFinanceSemester1module4.id);
 economicsFinanceSemester1.addModule(economicsFinanceSemester1module5.id);
 
-
 const economicsFinanceSemester2 = new StudyProgramSemester();
-const economicsFinanceSemester2module1 = new Module('Finansų pagrindai', 10)
-const economicsFinanceSemester2module2 = new Module('Makroekonomika I/III d.', 5)
-const economicsFinanceSemester2module3 = new Module('Matematiniai metodai II/II d.', 5)
-const economicsFinanceSemester2module4 = new Module('Mikroekonomika I/III d.', 5)
-const economicsFinanceSemester2module5 = new Module('Statistika', 5)
+const economicsFinanceSemester2module1 = new Module('Finansų pagrindai', 10);
+const economicsFinanceSemester2module2 = new Module('Makroekonomika I/III d.', 5);
+const economicsFinanceSemester2module3 = new Module('Matematiniai metodai II/II d.', 5);
+const economicsFinanceSemester2module4 = new Module('Mikroekonomika I/III d.', 5);
+const economicsFinanceSemester2module5 = new Module('Statistika', 5);
 economicsFinanceSemester2.addModule(economicsFinanceSemester2module1.id);
 economicsFinanceSemester2.addModule(economicsFinanceSemester2module2.id);
 economicsFinanceSemester2.addModule(economicsFinanceSemester2module3.id);
@@ -31,11 +30,11 @@ economicsFinanceSemester2.addModule(economicsFinanceSemester2module4.id);
 economicsFinanceSemester2.addModule(economicsFinanceSemester2module5.id);
 
 const economicsFinanceSemester3 = new StudyProgramSemester();
-const economicsFinanceSemester3module1 = new Module('Ekonometrija I/III d.', 5)
-const economicsFinanceSemester3module2 = new Module('Finansų rinkos', 5)
-const economicsFinanceSemester3module3 = new Module('Įmonių finansai', 5)
-const economicsFinanceSemester3module4 = new Module('Makroekonomika II/III d.', 5)
-const economicsFinanceSemester3module5 = new Module('Mikroekonomika II/III d.', 10)
+const economicsFinanceSemester3module1 = new Module('Ekonometrija I/III d.', 5);
+const economicsFinanceSemester3module2 = new Module('Finansų rinkos', 5);
+const economicsFinanceSemester3module3 = new Module('Įmonių finansai', 5);
+const economicsFinanceSemester3module4 = new Module('Makroekonomika II/III d.', 5);
+const economicsFinanceSemester3module5 = new Module('Mikroekonomika II/III d.', 10);
 economicsFinanceSemester3.addModule(economicsFinanceSemester3module1.id);
 economicsFinanceSemester3.addModule(economicsFinanceSemester3module2.id);
 economicsFinanceSemester3.addModule(economicsFinanceSemester3module3.id);
@@ -43,33 +42,31 @@ economicsFinanceSemester3.addModule(economicsFinanceSemester3module4.id);
 economicsFinanceSemester3.addModule(economicsFinanceSemester3module5.id);
 
 const economicsFinanceSemester4 = new StudyProgramSemester();
-const economicsFinanceSemester4module1 = new Module('Aplinkos ekonomika', 5)
-const economicsFinanceSemester4module2 = new Module('Bankininkystė', 10)
-const economicsFinanceSemester4module3 = new Module('Ekonometrija II/III d.', 5)
-const economicsFinanceSemester4module4 = new Module('Tarptautinė ekonomika', 5)
-const economicsFinanceSemester4module5 = new Module('Viešieji finansai', 5)
+const economicsFinanceSemester4module1 = new Module('Aplinkos ekonomika', 5);
+const economicsFinanceSemester4module2 = new Module('Bankininkystė', 10);
+const economicsFinanceSemester4module3 = new Module('Ekonometrija II/III d.', 5);
+const economicsFinanceSemester4module4 = new Module('Tarptautinė ekonomika', 5);
+const economicsFinanceSemester4module5 = new Module('Viešieji finansai', 5);
 economicsFinanceSemester4.addModule(economicsFinanceSemester4module1.id);
 economicsFinanceSemester4.addModule(economicsFinanceSemester4module2.id);
 economicsFinanceSemester4.addModule(economicsFinanceSemester4module3.id);
 economicsFinanceSemester4.addModule(economicsFinanceSemester4module4.id);
 economicsFinanceSemester4.addModule(economicsFinanceSemester4module5.id);
 
-
-
 const economicsFinanceSemester5 = new StudyProgramSemester();
-const economicsFinanceSemester5module1 = new Module('Ekonometrija III/III d.', 10)
-const economicsFinanceSemester5module2 = new Module('Mikroekonomika III/III d', 10)
-const economicsFinanceSemester5module3 = new Module('Tarptautiniai finansai', 10)
+const economicsFinanceSemester5module1 = new Module('Ekonometrija III/III d.', 10);
+const economicsFinanceSemester5module2 = new Module('Mikroekonomika III/III d', 10);
+const economicsFinanceSemester5module3 = new Module('Tarptautiniai finansai', 10);
 economicsFinanceSemester5.addModule(economicsFinanceSemester5module1.id);
 economicsFinanceSemester5.addModule(economicsFinanceSemester5module2.id);
 economicsFinanceSemester5.addModule(economicsFinanceSemester5module3.id);
 
 const economicsFinanceSemester6 = new StudyProgramSemester();
-const economicsFinanceSemester6module1 = new Module('Investicijų portfelio valdymas', 5)
-const economicsFinanceSemester6module2 = new Module('Makroekonomika III/III d.', 5)
-const economicsFinanceSemester6module3 = new Module('Finansinės technologijos', 10)
-const economicsFinanceSemester6module4 = new Module('Industrinė organizacija', 5)
-const economicsFinanceSemester6module5 = new Module('Sveikatos ekonomika', 5)
+const economicsFinanceSemester6module1 = new Module('Investicijų portfelio valdymas', 5);
+const economicsFinanceSemester6module2 = new Module('Makroekonomika III/III d.', 5);
+const economicsFinanceSemester6module3 = new Module('Finansinės technologijos', 10);
+const economicsFinanceSemester6module4 = new Module('Industrinė organizacija', 5);
+const economicsFinanceSemester6module5 = new Module('Sveikatos ekonomika', 5);
 economicsFinanceSemester6.addModule(economicsFinanceSemester6module1.id);
 economicsFinanceSemester6.addModule(economicsFinanceSemester6module2.id);
 economicsFinanceSemester6.addModule(economicsFinanceSemester6module3.id);
@@ -77,11 +74,11 @@ economicsFinanceSemester6.addModule(economicsFinanceSemester6module4.id);
 economicsFinanceSemester6.addModule(economicsFinanceSemester6module5.id);
 
 const economicsFinanceSemester7 = new StudyProgramSemester();
-const economicsFinanceSemester7module1 = new Module('Kursinis darbas', 10)
-const economicsFinanceSemester7module2 = new Module('Pinigų ir finansinio stabilumo politika', 5)
-const economicsFinanceSemester7module3 = new Module('Taikomoji ekonomika', 5)
-const economicsFinanceSemester7module4 = new Module('Darbo ekonomika', 5)
-const economicsFinanceSemester7module5 = new Module('Ekonominės minties istorija', 5)
+const economicsFinanceSemester7module1 = new Module('Kursinis darbas', 10);
+const economicsFinanceSemester7module2 = new Module('Pinigų ir finansinio stabilumo politika', 5);
+const economicsFinanceSemester7module3 = new Module('Taikomoji ekonomika', 5);
+const economicsFinanceSemester7module4 = new Module('Darbo ekonomika', 5);
+const economicsFinanceSemester7module5 = new Module('Ekonominės minties istorija', 5);
 economicsFinanceSemester7.addModule(economicsFinanceSemester7module1.id);
 economicsFinanceSemester7.addModule(economicsFinanceSemester7module2.id);
 economicsFinanceSemester7.addModule(economicsFinanceSemester7module3.id);
@@ -89,8 +86,8 @@ economicsFinanceSemester7.addModule(economicsFinanceSemester7module4.id);
 economicsFinanceSemester7.addModule(economicsFinanceSemester7module5.id);
 
 const economicsFinanceSemester8 = new StudyProgramSemester();
-const economicsFinanceSemester8module1 = new Module('Bakalauro baigiamasis darbas', 15)
-const economicsFinanceSemester8module2 = new Module('Profesinė praktika', 15)
+const economicsFinanceSemester8module1 = new Module('Bakalauro baigiamasis darbas', 15);
+const economicsFinanceSemester8module2 = new Module('Profesinė praktika', 15);
 economicsFinanceSemester8.addModule(economicsFinanceSemester8module1.id);
 economicsFinanceSemester8.addModule(economicsFinanceSemester8module2.id);
 
--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 import Student from "./entities/student.js";
 import softwareSystems from "./data/info-math/software-systems.js";
-import dataScience from './data/info-math/data-science.js';
-import mathematicsAndItsApplications from './data/info-math/mathematics-and-its-applications.js';
+import dataScience from "./data/info-math/data-science.js";
+import mathematicsAndItsApplications from "./data/info-math/mathematics-and-its-applications.js";
 import businessAndLaw from "./data/eco/business-law.js";
-import economicsFinance from './data/eco/economics-finance.js';
-import nanoChemistry from './data/chem/nano-chemistry.js';
-import odontology from './data/med/odontology.js';
+import economicsFinance from "./data/eco/economics-finance.js";
+import nanoChemistry from "./data/chem/nano-chemistry.js";
+import odontology from "./data/med/odontology.js";
 
 const studentsInfoAndMathSoftwareSystems = [
   new Student("Apsas", "Rekvestenis", softwareSystems.id),
@@ -22,42 +22,41 @@ const studentsInfoAndMathBusinessAndLaw = [
   new Student("Gintaras", "Ziba", businessAndLaw.id),
 ];
 const studentsInfoAndMathDataScience = [
-  new Student('Lina', 'Linaitė', dataScience.id),
-  new Student('Asta', 'Astaitė', dataScience.id),
-  new Student('Rokas', 'Rokaitis', dataScience.id),
-  new Student('Lukas', 'Lukaitis', dataScience.id),
-  new Student('Jonas', 'Jonaitis', dataScience.id),
+  new Student("Lina", "Linaitė", dataScience.id),
+  new Student("Asta", "Astaitė", dataScience.id),
+  new Student("Rokas", "Rokaitis", dataScience.id),
+  new Student("Lukas", "Lukaitis", dataScience.id),
+  new Student("Jonas", "Jonaitis", dataScience.id),
 ];
 const studentsInfoMathematicsAndItsApplications = [
-  new Student('Toushaing', 'Dluqum', mathematicsAndItsApplications.id),
-  new Student('Esoo', 'Prupia', mathematicsAndItsApplications.id),
-  new Student('Dha', 'Adeed', mathematicsAndItsApplications.id),
-  new Student('Caxeo', 'Pruvondun', mathematicsAndItsApplications.id),
-  new Student('Hawan', 'Baintai', mathematicsAndItsApplications.id),
+  new Student("Toushaing", "Dluqum", mathematicsAndItsApplications.id),
+  new Student("Esoo", "Prupia", mathematicsAndItsApplications.id),
+  new Student("Dha", "Adeed", mathematicsAndItsApplications.id),
+  new Student("Caxeo", "Pruvondun", mathematicsAndItsApplications.id),
+  new Student("Hawan", "Baintai", mathematicsAndItsApplications.id),
 ];
 const studentsChemNanoChemistry = [
-  new Student('Tomas', 'Ratkauskas', nanoChemistry.id),
-  new Student('Ponas', 'BMWas', nanoChemistry.id),
-  new Student('Opelis', 'Servisas', nanoChemistry.id),
-  new Student('Audi', 'Spaudi', nanoChemistry.id),
-  new Student('Voltas', 'Voldemortas', nanoChemistry.id),
+  new Student("Tomas", "Ratkauskas", nanoChemistry.id),
+  new Student("Ponas", "BMWas", nanoChemistry.id),
+  new Student("Opelis", "Servisas", nanoChemistry.id),
+  new Student("Audi", "Spaudi", nanoChemistry.id),
+  new Student("Voltas", "Voldemortas", nanoChemistry.id),
 ];
 const studentsOdontology = [
-  new Student('Hari', 'Krisna', odontology.id),
-  new Student('Maiklas', 'Dziordonas', odontology.id),
-  new Student('Vardenis','Pavardenis', odontology.id),
-  new Student('Agrastas', 'Kaspinuotis', odontology.id),
-  new Student('Jageris', 'Maisteris', odontology.id),
+  new Student("Hari", "Krisna", odontology.id),
+  new Student("Maiklas", "Dziordonas", odontology.id),
+  new Student("Vardenis", "Pavardenis", odontology.id),
+  new Student("Agrastas", "Kaspinuotis", odontology.id),
+  new Student("Jageris", "Maisteris", odontology.id),
 ];
 
-const studentsEconomicsFinance =[
-  new Student('Kazimira Danutė' , 'Prunskienė', economicsFinance.id ),
-  new Student('Algirdas Mykolas' , 'Brazauskas', economicsFinance.id ),
-  new Student('Česlovas' , 'Juršėnas', economicsFinance.id ),
-  new Student('Rolandas' , 'Paksas', economicsFinance.id ),
-  new Student('Irena' , 'Degutienė', economicsFinance.id ),
-]
-
+const studentsEconomicsFinance = [
+  new Student("Kazimira Danutė", "Prunskienė", economicsFinance.id),
+  new Student("Algirdas Mykolas", "Brazauskas", economicsFinance.id),
+  new Student("Česlovas", "Juršėnas", economicsFinance.id),
+  new Student("Rolandas", "Paksas", economicsFinance.id),
+  new Student("Irena", "Degutienė", economicsFinance.id),
+];
 
 console.table(studentsInfoAndMathSoftwareSystems);
 console.table(studentsInfoAndMathBusinessAndLaw);
