570130237cbbda61daf296634a17e6faf9cd59a4
karolisjon <karolisjonn@gmail.com>
2022-05-23 18:16:07 +0300

    chore: unified import order

--- js/data/modules.js
+++ js/data/modules.js
@@ -1,14 +1,14 @@
-import { odontologyModules } from './med/odontology.js';
 import { softwareSystemsModules } from './info-math/software-systems.js';
 import { mathematicsAndItsApplicationsModules } from './info-math/mathematics-and-its-applications.js';
 import { dataScienceModules } from './info-math/data-science.js';
 import { economicsAndBusinessModules } from "./eco/business-law.js";
+import { odontologyModules } from './med/odontology.js';
 
 const modules = [
   ...softwareSystemsModules,
+  ...mathematicsAndItsApplicationsModules,
   ...dataScienceModules,
   ...economicsAndBusinessModules,
-  ...mathematicsAndItsApplicationsModules,
   ...odontologyModules,
 ];
 
