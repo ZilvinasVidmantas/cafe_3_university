b003b936ee9fa49f14c853ec34dc9f0abe1e4682
MazvydasV <mvaicekauskas@inbox.lt>
2022-05-23 19:25:15 +0300

    feat: renamed faculty

--- js/data/faculties.js
+++ js/data/faculties.js
@@ -1,12 +1,12 @@
 import Faculty from '../entities/faculty.js';
 
-
 export const infoAndMathFaculty = new Faculty('Informatikos ir matematikos fakultetas');
 export const chemistryAndGeosciencesFaculty = new Faculty ('Chemijos ir geomokslų fakultetas');
 
 const faculties = [
   infoAndMathFaculty,
   chemistryAndGeosciencesFaculty,
+  infoAndMathFaculty,
 ];
 
 export default faculties;
