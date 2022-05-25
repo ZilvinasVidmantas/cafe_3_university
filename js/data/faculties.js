2ce94a7832f92680ec633c7e7841f0325217feac
Ema Alsv <alsevskaja.ema@gmail.com>
2022-05-25 21:30:07 +0300

    pataisyta

--- js/data/faculties.js
+++ js/data/faculties.js
@@ -1,12 +1,11 @@
 import Faculty from '../entities/faculty.js';
 
-const faculties = [
-    philosophyFaculty,
-];
+const faculties = [];
 
 export const informaticsFaculty = new Faculty('Informatikos fakultetas');
 export const philosophyFaculty = new Faculty('Filosofijos fakultetas');
 
 faculties.push(informaticsFaculty);
+faculties.push(philosophyFaculty);
 
 export default faculties;
